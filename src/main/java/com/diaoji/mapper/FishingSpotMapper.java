f7539f0e4b3494c15c523e7ea5454933e0390c97
77595644 <77595644@qq.com>
2026-04-20 12:16:55 +0800

    Add src/main/java/com/diaoji/mapper/FishingSpotMapper.java

---
+++ src/main/java/com/diaoji/mapper/FishingSpotMapper.java
@@ -0,0 +1,9 @@
+package com.diaoji.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.diaoji.entity.FishingSpot;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface FishingSpotMapper extends BaseMapper<FishingSpot> {
+}
